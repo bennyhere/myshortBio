9471ff1180583770497c978c0efee660dd733daa
bennyhere <135466845+bennyhere@users.noreply.github.com>
2023-06-03 23:26:13 +0200

    Update user.js

--- story_content/user.js
+++ story_content/user.js
@@ -18,7 +18,7 @@ const timeSpent = player.GetVar("NumericEntry");//Storyline variables assigned t
 //alert("Hello");
 alert("Thank you for graciously investing "+(timeSpent/1000)+"  seconds of your precious time to read about me until now. If you're intrigued to learn more, I invite you to click on my LinkedIn link.");
 var xhr = new XMLHttpRequest();
-xhr.open("POST", "https://prod-175.westeurope.logic.azure.com:443/workflows/13cd8a1f411f44039f1f6fe13a5b7781/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=Dla8uEoCmZYeSXvBTB4MnI6fl76fa6H7T74N3dludTo", true); 
+xhr.open("POST", "url", true); 
 // Replace with your endpoint URL
 xhr.setRequestHeader("Content-Type", "application/json");
 var data = JSON.stringify(
